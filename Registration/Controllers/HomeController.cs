329ad380514b60c1e3aa6273498f4f97ff143a47
BrettMerri <the.b.merri@gmail.com>
2017-07-31 20:13:55 -0400

    Initial commit

---
+++ Registration/Controllers/HomeController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Registration.Models;
+
+namespace Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<US_State> statesList = StateArray.ToList(); //Gets list of states for the Select list in the view
+            ViewBag.StatesList = statesList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string firstName, string lastName, string address1,
+            string address2, string city, string state, string zipcode, string country)
+        {
+            List<string> statesList = StateArray.Abbreviations(); //Gets list of state abbreviations
+
+            //First name
+            if (string.IsNullOrWhiteSpace(firstName) ||
+                firstName.Length > 50)
+            {
+                ViewBag.ErrorMessage = $"Invalid first name: {firstName}";
+                return View("Error");
+            }
+
+            //Last name
+            if (string.IsNullOrWhiteSpace(lastName) ||
+             lastName.Length > 50)
+            {
+                ViewBag.ErrorMessage = $"Invalid last name: {lastName}";
+                return View("Error");
+            }
+
+            //Address1
+            if (string.IsNullOrWhiteSpace(address1) ||
+             address1.Length > 100)
+            {
+                ViewBag.ErrorMessage = $"Invalid address1: {address1}";
+                return View("Error");
+            }
+
+            //Address2
+            if (!string.IsNullOrWhiteSpace(address2) &&
+             address2.Length > 100)
+            {
+                ViewBag.ErrorMessage = $"Invalid address2: {address2}";
+                return View("Error");
+            }
+
+            //City
+            if (string.IsNullOrWhiteSpace(city) ||
+            city.Length > 50)
+            {
+                ViewBag.ErrorMessage = $"Invalid city: {city}";
+                return View("Error");
+            }
+
+            //State
+            if (string.IsNullOrWhiteSpace(state) ||
+            state.Length != 2 ||
+            !statesList.Contains(state))
+            {
+                ViewBag.ErrorMessage = $"Invalid state: {state}";
+                return View("Error");
+            }
+
+            //Zipcode
+            if (string.IsNullOrWhiteSpace(zipcode) ||
+                zipcode.Length != 5 ||
+                !zipcode.All(char.IsDigit))
+            {
+                ViewBag.ErrorMessage = $"Invalid zipcode: {zipcode}";
+                return View("Error");
+            }
+
+            //Set address2 to null if empty
+            if (string.IsNullOrWhiteSpace(address2))
+                address2 = null;
+
+            try
+            {
+                RegistrationEntities db = new RegistrationEntities();
+
+                User newUser = new User()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Address1 = address1,
+                    Address2 = address2,
+                    City = city,
+                    State = state,
+                    Zipcode = int.Parse(zipcode), //Should be all digits so TryParse is not needed
+                    Country = "US", //There is no way to register without being from the US
+                    DateTime = DateTime.Now
+                };
+
+                db.Users.Add(newUser);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = $"Error saving to database. {ex.ToString()}";
+                return View("Error");
+            }
+            
+
+            return RedirectToAction("Confirmation");
+        }
+
+        public ActionResult Confirmation()
+        {
+            return View();
+        }
+
+        public ActionResult Admin()
+        {
+            RegistrationEntities db = new RegistrationEntities();
+            List<User> userList = db.Users.OrderByDescending(x => x.DateTime).ToList();
+            return View(userList);
+        }
+    }
+}
---
+++ Registration/Scripts/index.js
@@ -0,0 +1,106 @@
+﻿$(document).ready(function() {
+    $('#register-form').submit(function (e) {
+        
+        clearAllSpans(); //Clears all the validation text before revalidating fields
+
+        var validationErrors = 0;
+
+        //Get form values
+        var firstName = $('#firstName').val();
+        var lastName = $('#lastName').val();
+        var address1 = $('#address1').val();
+        var address2 = $('#address2').val();
+        var city = $('#city').val();
+        var state = $('#state').val();
+        var zipcode = $('#zipcode').val();
+        var country = $('input[name=country]:checked').val(); //value will be "yes" if United States
+
+        //First name
+        if (firstName.length === 0) {
+            $("#firstNameSpan").text("First name is required");
+            validationErrors++;
+        }
+        else if (firstName.length > 50) {
+            $("#firstNameSpan").text("First name must be less than 50 characters");
+            validationErrors++;
+        }
+
+        //Last name
+        if (lastName.length === 0) {
+            $("#lastNameSpan").text("Last name is required");
+            validationErrors++;
+        }
+        else if (lastName.length > 50) {
+            $("#lastNameSpan").text("Last name must be less than 50 characters");
+            validationErrors++;
+        }
+
+        //Address 1
+        if (address1.length === 0) {
+            $("#address1Span").text("Address is required");
+            validationErrors++;
+        }
+        else if (address1.length > 100) {
+            $("#address1Span").text("Address must be less than 100 characters");
+            validationErrors++;
+        }
+
+        //Address 2
+        if (address2.length !== 0 && address2.length > 100) {
+            $("#address2Span").text("Address must be less than 100 characters");
+            validationErrors++;
+        }
+
+        //City
+        if (city.length === 0) {
+            $("#citySpan").text("City is required");
+            validationErrors++;
+        }
+        else if (city.length > 50) {
+            $("#citySpan").text("City must be less than 50 characters");
+            validationErrors++;
+        }
+
+        //State
+        if (state.length === 0) {
+            $("#stateSpan").text("State is required");
+            validationErrors++;
+        }
+
+        //Zipcode
+        if (zipcode.length === 0) {
+            $("#zipcodeSpan").text("Zipcode is required");
+            validationErrors++;
+        }
+        else if (!$.isNumeric(zipcode)) {
+            $("#zipcodeSpan").text("Zipcode must be only digits");
+            validationErrors++;
+        }
+        else if (zipcode.length !== 5) {
+            $("#zipcodeSpan").text("Zipcode must be 5 digits long");
+            validationErrors++;
+        }
+
+        //Country
+        if (country !== "yes") {
+            $("#countrySpan").text("You must live in the United States to register");
+            validationErrors++;
+        }
+
+        if (validationErrors === 0)
+            return true; //If there are no validation errors, submit form
+
+        return false; //This prevents the form from submitting
+    });
+
+    function clearAllSpans()
+    {
+        $("#firstNameSpan").text("");
+        $("#lastNameSpan").text("");
+        $("#addressSpan").text("");
+        $("#citySpan").text("");
+        $("#stateSpan").text("");
+        $("#zipcodeSpan").text("");
+        $("#countrySpan").text("");
+    }
+});
